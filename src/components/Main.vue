c150ef047aaeeead2a123ec248890e66b0c8f0f3
Oriol Vall <oriol.vall.hdez@gmail.com>
2018-03-03 10:45:51 +0100

    map improvements

---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Map></Map>
+  </div>
+</template>
+
+<script>
+import Map from './Map'
+
+export default {
+  name: 'Main',
+  components: {
+    Map
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .in-front{
+    position: absolute;
+    z-index:9999;
+  }
+
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="myMap" class="map">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Map',
+  data () {
+    return {
+      myMap: null
+    }
+  },
+  mounted () {
+    var L = window.L
+    this.myMap = L.map('myMap', {
+      zoomControl: false
+    }).setView([0, 0], 2)
+
+    // add zoom control with your options
+    L.control.zoom({
+      position: 'bottomright'
+    }).addTo(this.myMap)
+
+    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+      maxZoom: 19
+    }).addTo(this.myMap)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.map{
+  height:100vh;
+}
+</style>
